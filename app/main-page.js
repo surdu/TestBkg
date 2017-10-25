52a1eaa1a8367304730e116dbfde3f0fe3e20184
surdu <postal2600@gmail.com>
2017-10-25 11:13:03 +0300

    Even more simplification + small fix

--- app/main-page.js
+++ app/main-page.js
@@ -2,11 +2,10 @@ var index = 0;
 
 function loaded(args) {
   const webview = args.object;
-  console.log("webview.src:", webview.src);
-  console.log("index:", index);
   if (index == 0) {
     webview.src = "https://nativescript.org";
   }
+  index += 1;
 }
 
 exports.loaded = loaded;
